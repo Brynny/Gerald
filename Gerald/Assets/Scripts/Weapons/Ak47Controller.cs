7d59a5555d46ea1dbe7a29c168a3991d9f7335ff
Brynny <111567@student.sae.edu.au>
2016-12-17 13:03:18 +0800

    Added bullet spawn
    Won't Register collision

---
+++ Gerald/Assets/Scripts/Weapons/Ak47Controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ak47Controller : MonoBehaviour 
+{
+
+	public Rigidbody bulletPrefab;
+	Rigidbody bulletInstance;
+	public Transform AssaultRifleGun;
+	float timer = 0.25f;
+
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		Shoot();
+	}
+
+	void Shoot ()
+	{
+		timer = timer - Time.deltaTime;
+
+		if (Input.GetMouseButton (0) && timer <= 0)
+			{
+				bulletInstance = Instantiate (bulletPrefab, AssaultRifleGun.position, AssaultRifleGun.rotation) as Rigidbody;
+			timer = 0.25f;
+			}
+	}
+}
---
+++ Gerald/Assets/Scripts/Weapons/BulletController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour 
+{
+	public float moveSpeed;
+	public Rigidbody bullet;
+	float timer = 1f;
+
+
+	void Start () 
+	{
+		bullet = GetComponent<Rigidbody>();
+	}
+	
+
+	void Update ()
+	{
+	
+	}
+
+	void FixedUpdate()
+	{
+		BulletMovement();
+		//Self destruct bullet if not destroyed within 3 seconds
+		timer -= Time.deltaTime;
+		if (timer <= 0) 
+		{
+			Destroy (this.gameObject);
+		}
+	}
+
+	void BulletMovement()
+	{
+		//Give bullet movement to move in a forward direction.
+		bullet.velocity = transform.forward * moveSpeed;
+	}
+
+	void OntriggerEnter(Collider other)
+	{
+		if (other.gameObject) 
+		{
+			Debug.Log("Collider with "+other.gameObject.name);
+			Destroy (this.gameObject);
+
+		}
+	}
+}
