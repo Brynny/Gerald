df47bc0c3f870a12a29362c805a2e67cbeb62e14
Brynny <111567@student.sae.edu.au>
2016-12-18 16:50:58 +0800

    Started AI

---
+++ Gerald/Assets/Scripts/AIAlertController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIAlertController : MonoBehaviour
+{
+	public GameObject Ai;
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Ai.GetComponent<AIMovementController>().MoveToPlayer();
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Ai.GetComponent<AIMovementController>().StopMovement();
+		}
+	}
+}
---
+++ Gerald/Assets/Scripts/AIMovementController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovementController : MonoBehaviour
+{
+	public Transform target;
+
+	private NavMeshAgent agent;
+
+
+	void Start () 
+	{
+		agent = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+
+	void FixedUpdate ()
+	{
+		
+	}
+
+	public void MoveToPlayer()
+	{
+		agent.SetDestination(target.position);
+	}
+
+	public void StopMovement()
+	{
+		agent.Stop(true);
+	}
+}
