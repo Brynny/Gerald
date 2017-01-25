bece219e3c2de1fda80a5d1c2eb42842e69c48f1
Brynny <111567@student.sae.edu.au>
2017-01-25 13:52:56 +0800

    added cabin
    Cabin roof gameobject disables when the player enters the cabin to see
    inside and re enables when you leave the cabin. So happy xD

---
+++ Gerald/Assets/Scripts/ForestLevel/Buildings/DisableGameobject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableGameobject : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void DisableObject()
+	{
+		gameObject.SetActive(false);
+	}
+
+	public void EnableObject()
+	{
+		gameObject.SetActive(true);
+	}
+}
---
+++ Gerald/Assets/Scripts/ForestLevel/Buildings/DisableGameobjectTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableGameobjectTrigger : MonoBehaviour
+{
+	public GameObject disabledGameobject;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Debug.Log("HIT TRIGGER");
+			disabledGameobject.GetComponent<DisableGameobject>().DisableObject();
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Debug.Log("LEFT TRIGGER");
+			disabledGameobject.GetComponent<DisableGameobject>().EnableObject();
+		}
+	}
+}
