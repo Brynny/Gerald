ea4292a369f4e1edcb34793f8b9fa06b63940c13
Brynny <111567@student.sae.edu.au>
2017-01-27 10:05:25 +0800

    save

---
+++ Gerald/Assets/Scripts/MiniMapCameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapCameraController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
