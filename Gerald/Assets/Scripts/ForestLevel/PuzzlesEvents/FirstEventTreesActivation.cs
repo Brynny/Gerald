4edbeebc33f7cdf0b655869bbf80939d41788d91
Brynny <111567@student.sae.edu.au>
2017-02-02 21:17:10 +0800

    Started FirstEvent, Added GeraldCyborg Character

---
+++ Gerald/Assets/Scripts/ForestLevel/PuzzlesEvents/FirstEventTreesActivation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstEventTreesActivation : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void DisableTrees()
+	{
+		gameObject.SetActive(true);
+	}
+}
---
+++ Gerald/Assets/Scripts/ForestLevel/PuzzlesEvents/FirstEventTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstEventTrigger : MonoBehaviour 
+{
+	public GameObject TreesGO;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			TreesGO.GetComponent<FirstEventTreesActivation>().DisableTrees();
+		}
+	}
+
+}
