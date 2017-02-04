e9a051bd3a8cc28c7770196525343748ff4b7add
Brynny <111567@student.sae.edu.au>
2017-02-04 22:00:02 +0800

    added UI text for first event

--- Gerald/Assets/Scripts/ForestLevel/PuzzlesEvents/FirstEvent/FirstEventTrigger.cs
+++ Gerald/Assets/Scripts/ForestLevel/PuzzlesEvents/FirstEvent/FirstEventTrigger.cs
@@ -1,16 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FirstEventTrigger : MonoBehaviour 
 {
-	public GameObject TreesGO;
+	public Text FirstEventIntroText;
+	public float timer = 5f;
+	bool startTimer = false;
+
+	void Start()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		if (startTimer == true)
+		{
+		timer -= Time.deltaTime;
+		}
+
+		if (timer <= 0)
+		{
+			Destroy(FirstEventIntroText);
+			timer = 0;
+		}
+	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
-			TreesGO.GetComponent<FirstEventTreesActivation>().DisableTrees();
+			FirstEventIntroText.text = "What a strange colour rock......";
+			startTimer = true;
 		}
 	}
-
 }
