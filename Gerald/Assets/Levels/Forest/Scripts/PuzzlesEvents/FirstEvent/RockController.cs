36192d4d5966ba33833bead7f21497e86714bad2
Brynny <brynnystudios@gmail.com>
2017-03-27 23:40:08 +0800

    save
    started changing first puzzle rock so that when the player is close
    enough to trigger removing the rock, it destroys, however currently not
    working.

---
+++ Gerald/Assets/Levels/Forest/Scripts/PuzzlesEvents/FirstEvent/RockController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockController : MonoBehaviour 
+{
+	public float rockHP = 10f;
+
+//	void FixedUpdate()
+//	{
+//		if (rockHP <= 0)
+//		{
+//			Destroy (this.gameObject);
+//		}
+//	}
+//
+//	void OnTriggerEnter(Collider other)
+//	{
+//		if (other.gameObject.tag == "Bullet")
+//		{
+//			rockHP -= 1f;
+//		}
+//	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player" && Input.GetKeyDown (KeyCode.E))
+		{
+			Debug.Log ("Player Is Here");
+			Destroy (this.gameObject);
+		}
+	}
+
+}
+
